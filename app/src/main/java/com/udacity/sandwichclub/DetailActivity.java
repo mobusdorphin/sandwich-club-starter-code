52102cf01fea1d9cad91fef925996afd1820bc6e
Mobus Dorphin <celestialtuba@gmail.com>
2018-04-26 06:26:48 -0400

    First release of Sandwich Club

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,24 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -36,14 +48,19 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +73,49 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView originTextView = (TextView) findViewById(R.id.origin_tv);
+        TextView descriptionTextView = (TextView) findViewById(R.id.description_tv);
+        LinearLayout akaList = (LinearLayout) findViewById(R.id.aka_ll);
+        LinearLayout ingredientList = (LinearLayout) findViewById(R.id.ingredients_ll);
+//        ListView akaListView = (ListView) findViewById(R.id.also_known_lv);
+
+        originTextView.setText(
+                sandwich.getPlaceOfOrigin().equals("")?"Unknown":sandwich.getPlaceOfOrigin());
+        descriptionTextView.setText(sandwich.getDescription());
+        if (!sandwich.getAlsoKnownAs().isEmpty()) {
+            for (String s : sandwich.getAlsoKnownAs()) {
+                TextView textView = new TextView(this);
+                textView.setText(s);
+                akaList.addView(textView);
+            }
+        } else {
+            TextView textView = new TextView(this);
+            textView.setText("N/A");
+            akaList.addView(textView);
+        }
+
+        if (!sandwich.getIngredients().isEmpty()) {
+            for (String s : sandwich.getIngredients()) {
+                TextView textView = new TextView(this);
+                textView.setText(s);
+                ingredientList.addView(textView);
+            }
+        } else {
+            TextView textView = new TextView(this);
+            textView.setText("N/A");
+            ingredientList.addView(textView);
+        }
+///        List<String> ingredientArray = (!sandwich.getIngredients().isEmpty()) ?
+//                sandwich.getIngredients() : Collections.singletonList("N/A");
+//        ingredientsListView.setAdapter(new ArrayAdapter<>(this,
+//                            android.R.layout.simple_list_item_1, ingredientArray));
+
+//        List<String> akaArray = (!sandwich.getAlsoKnownAs().isEmpty())?
+//                sandwich.getAlsoKnownAs() : Collections.singletonList("N/A");
+//        akaListView.setAdapter(new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, akaArray));
 
     }
+
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,50 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        String mainName;
+        List<String> alsoKnownAs = new ArrayList<String>();
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients = new ArrayList<String>();
+
+        final String SANDWICH_NAME = "name";
+        final String SANDWICH_MAIN_NAME = "mainName";
+        final String SANDWICH_AKA = "alsoKnownAs";
+        final String SANDWICH_ORIGIN = "placeOfOrigin";
+        final String SANDWICH_DESCRIPTION = "description";
+        final String SANDWICH_IMG = "image";
+        final String SANDWICH_INGREDIENTS = "ingredients";
+
+        JSONObject sandwichJson = new JSONObject(json);
+        JSONObject name = sandwichJson.getJSONObject(SANDWICH_NAME);
+        mainName = name.getString(SANDWICH_MAIN_NAME);
+        JSONArray akaArray = name.getJSONArray(SANDWICH_AKA);
+        if (akaArray.length() > 0) {
+            for (int i = 0; i < akaArray.length(); i++) {
+                alsoKnownAs.add(akaArray.getString(i));
+            }
+        }
+        placeOfOrigin = sandwichJson.getString(SANDWICH_ORIGIN);
+        description = sandwichJson.getString(SANDWICH_DESCRIPTION);
+        image = sandwichJson.getString(SANDWICH_IMG);
+        JSONArray ingredientArray = sandwichJson.getJSONArray(SANDWICH_INGREDIENTS);
+        if (ingredientArray.length() > 0) {
+            for (int i = 0; i < ingredientArray.length(); i++) {
+                ingredients.add(ingredientArray.getString(i));
+            }
+        }
+
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 }
